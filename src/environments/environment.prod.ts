56a8439f87476cad39954b5c2f6ed0bbc161687e
EryonS <lucas.schilling.pro@gmail.com>
2021-04-20 23:02:39 +0200

    Add express

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  LOGIN_URL: 'https://main-botmins-mipug91y2fga6d9l-gtw.qovery.io/login',
+  HOME_URL: 'https://main-botmins-mipug91y2fga6d9l-gtw.qovery.io/home',
+  TWEET_URL: 'https://main-botmins-mipug91y2fga6d9l-gtw.qovery.io/tweet',
 };
